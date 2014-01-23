b6db789e8bcf180cb409a6b00cc29cf14c07e21b
crccheck <c@crccheck.com>
2014-01-22 22:17:25 -0600

    first pass

---
+++ main.py
@@ -0,0 +1,70 @@
+from os import environ as env
+import logging
+
+from pymarkovchain import MarkovChain
+from lxml import html
+import project_runpy
+import requests
+import tweepy
+
+
+HOST = 'http://www.texastribune.org'
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(project_runpy.ColorizingStreamHandler())
+
+
+def get_tweet_text(mc):
+    text = mc.generateString()
+    for __ in range(10):  # only try 10 times
+        if len(text) > 140:
+            logger.warn(u'Too Long: {}'.format(text))
+            text = mc.generateString()
+    return text
+
+
+def send_tweet(text):
+    auth = tweepy.OAuthHandler(
+            env.get('CONSUMER_KEY'), env.get('CONSUMER_SECRET'))
+    auth.set_access_token(
+            env.get('ACCESS_KEY'), env.get('ACCESS_SECRET'))
+    api = tweepy.API(auth)
+    api.update_status(text)
+
+
+if __name__ == '__main__':
+    page = requests.get(HOST + '/')
+    tree = html.fromstring(page.text)
+
+    comment_links = tree.xpath("//a[@class='comments']/@href")
+
+    all_comments = []
+    links_retrieved = set()
+
+    for link in comment_links:
+        if link in links_retrieved:
+            logger.info('Skipping {}'.format(link))
+            continue
+        links_retrieved.add(link)
+        logger.info('Retrieving {}'.format(link))
+        page = requests.get(HOST + link, timeout=2)
+        tree = html.fromstring(page.text)
+        # this may be splitting comments into multiples if there are line
+        # breaks, but oh well
+        comments = tree.xpath("//p[@class='comment']/text()")
+
+        # throw away comments with urls
+        # cast to a standard type isntead of lxml.etree._ElementStringResult
+        comments = [unicode(x) for x in comments if 'http' not in x]
+
+        all_comments.extend(comments)
+
+
+    mc = MarkovChain('/tmp/temp.db')
+    mc.db = {}  # HACK to clear any existing data, we want to stay fresh
+    mc.generateDatabase(
+        # seems silly to join and then immediately split, but oh well
+        '\n'.join(all_comments),
+        sentenceSep='[\n]',
+    )
+    print get_tweet_text(mc)
